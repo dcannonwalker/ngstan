fc8cdb8ea342f8a0747c4d5b42ce96456945935d
dcannonwalker <dcwalker@iastate.edu>
2024-11-23 23:10:49 -0600

    additional deps???

--- R/data.R
+++ R/data.R
@@ -7,7 +7,8 @@
 #' A data frame with 27,628 rows and 37 columns:
 #' \describe{
 #'   \item{geneid}{Gene identifier}
-#'   \item{ribo_..., rna_...}{Ribo-Seq and RNA-Seq counts for treatment and control samples at 5 and 8 days post infection}
+#'   \item{ribo_..., rna_...}{Ribo-Seq and RNA-Seq counts for treatment
+#'   and control samples at 5 and 8 days post infection}
 #' }
 #' @source <https://www.ncbi.nlm.nih.gov/bioproject/PRJNA950066>
 "dte"
