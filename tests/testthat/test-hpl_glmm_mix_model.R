ce9edb7de73b0995abbb3e441cd0d3822b346db7
dcannonwalker <dcwalker@iastate.edu>
2024-11-27 20:30:07 -0600

    very simple test

--- tests/testthat/test-hpl_glmm_mix_model.R
+++ tests/testthat/test-hpl_glmm_mix_model.R
@@ -1,21 +1,8 @@
 cmdstan_version <- try(cmdstanr::cmdstan_version(), silent = TRUE)
 found_cmdstan <- inherits(cmdstan_version, "try-error")
-test_that("test skips if no cmdstan install on system", {
-  skip_if_not(found_cmdstan)
-})
+message(found_cmdstan)
 
-# setup args ----
-
-# test that simulated response has expected structure ----
 test_that("hpl_glmm_mix simulates data with the expected structure", {
   skip_if_not(found_cmdstan)
-  expect_equal(dim(y_sim_single), c(args$G, nrow(args$X_g)))
-})
-
-# fit the model to the simulated data set ----
-
-# test that model fits simulated data without error
-test_that("model fits without error", {
-  skip_if_not(found_cmdstan)
-  expect_no_error(do.call(run_hpl_glmm_mix_model, args = sample_args))
+  expect_no_error(cmdstanr::cmdstan_path())
 })
