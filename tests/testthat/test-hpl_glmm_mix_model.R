290ec052455f35f405c58518fd77c772d07ae184
dcannonwalker <dcwalker@iastate.edu>
2024-11-27 20:46:07 -0600

    typo

--- tests/testthat/test-hpl_glmm_mix_model.R
+++ tests/testthat/test-hpl_glmm_mix_model.R
@@ -4,6 +4,6 @@ message(found_cmdstan)
 
 test_that("hpl_glmm_mix simulates data with the expected structure", {
   skip_if_not(found_cmdstan)
-  set_cmdstan_path()
+  cmdstanr::set_cmdstan_path()
   expect_no_error(cmdstanr::cmdstan_path())
 })
