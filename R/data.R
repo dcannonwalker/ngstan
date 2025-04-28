417ad322c954a4def0a973fcff249becb29bcc8b
dcannonwalker <42654459+dcannonwalker@users.noreply.github.com>
2025-04-28 06:09:42 -0500

    document tuch_tumor

--- R/data.R
+++ R/data.R
@@ -26,3 +26,19 @@
 #' }
 #' @source <https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE50499>
 "mov10"
+
+#' Tuch tumor data
+#'
+#' Counts per gene
+#'
+#' @format ## `tuch_tumor`
+#' A data frame with 15,668 rows and 9 columns:
+#' \describe{
+#'  \item{id_ref_seq}{Gene identifier}
+#'  \item{name_of_gene}{Gene name}
+#'  \item{number_of_exons}{Exons per gene}
+#'  \item{8n, 8t, 33n, 33t, 51n, 51t}{Gene expression counts
+#'  for each sample}
+#' }
+#' @source <https://journals.plos.org/plosone/article?id=10.1371/journal.pone.0009317#s5>
+"tuch_tumor"
